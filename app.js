78aec25d7b3fdac9bb59c347898ebc7d7c51bbd2
Willian Carvalho <72278301+Williams26-master@users.noreply.github.com>
2023-04-19 22:07:30 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let galeria = document.getElementById('galeria')
+
+function criargaleria() {
+for(let i = 0; i < dados.length; i++){
+console.log('oi')
+
+        galeria.innerHTML += `
+    <div class="col-lg-3 col-md-4 col-6 mb-3">
+
+    <div class="card bg-dark">
+        <img src="${dados[i].url}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${dados[i].titulo}</h5>
+            <p class="card-text">${dados[i].descricao.substring(0,100)}</p>
+            <p class="card-text">${dados[i].descricao.toUpperCase('')}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>   
+</div>
+    `
+    }
+    // console.log(dados)
+}
+
+criargaleria()
